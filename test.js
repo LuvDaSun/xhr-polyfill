292a7a03f5d91be258c2c9e608e67935bf7d7beb
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-06-21 23:40:04 +0200

    fixing test runner

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var child_process = require('child_process');
 
 process.chdir(__dirname);
 
-child_process.execFile('server.js');
+child_process.spawn('node', ['server'], {});
 
 karma.server.start({
 	configFile: 'karma.conf.js'
