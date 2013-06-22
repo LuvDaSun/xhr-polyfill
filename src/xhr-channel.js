9f94f9638a1d66fb7f4e674224eddf65ed3b5699
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-06-22 13:24:38 +0200

    renaming

---
+++ src/xhr-channel.js
@@ -0,0 +1,52 @@
+bindEvent(window, 'message', function(e){
+	var message;
+
+	if(!(message = receiveMessage(e, window.parent))) return;
+
+	xhr(message, function(state) {
+		window.parent.postMessage(JSON.stringify(state), '*');
+	});
+});
+
+
+function xhr(options, statechange){
+	var headerName;
+	
+	var xhr = new (window.XMLHttpRequest || function() {
+		try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch (e1) {}
+		try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch (e2) {}
+		try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e3) {}
+		throw new Error("This browser does not support XMLHttpRequest.");
+	})()
+
+	xhr.onreadystatechange = function(){
+		var state = {};
+		state.id = options.id
+		state.readyState = this.readyState;
+		switch(this.readyState){
+
+			case 1:
+			case 2:
+			case 3:
+			break;
+
+			case 4:
+			state.responseBody = this.responseText;
+			state.responseHeaders = this.getAllResponseHeaders();
+			state.statusCode = this.status;
+			state.statusText = this.statusText;
+			break;
+
+			default:
+			throw 'invalid state'
+		}
+		statechange(state)
+	}
+	xhr.open(options.method, options.url, true, options.username, options.password);
+	if(options.requestHeaders) {
+		for(headerName in parseHeaders(options.requestHeaders)) {
+			xhr.setRequestHeader(headerName, options.requestHeaders[headerName]);
+		}
+	}
+	xhr.send(options.requestBody);
+}//window_onmessage
