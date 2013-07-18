56f8b55369fef6734fbe5d65186f3c4c51593143
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-07-18 10:29:15 +0200

    watch src folder add proxy for testing local xhr requests

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,3 @@
-basePath = '';
-
 files = [
 	MOCHA, MOCHA_ADAPTER
 	, 'mocha_setup.js'
@@ -7,7 +5,12 @@ files = [
 	, 'components/jquery/jquery.js'
 	, 'src/shared.js', 'src/IFrameChannel.js', 'src/XMLHttpRequestProxy.js'
 	, 'test/**.js'
+	,  {pattern: 'src/**/*', watched: true, included: false, served: false}
 ];
+
+proxies = {
+  '/local/': 'http://localhost:8080/'
+};
 
 // Start these browsers, currently available:
 // - Chrome
